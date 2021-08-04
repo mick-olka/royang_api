a00019fbcd28174536c45b472a78290ca4304224
mick-olka <mickolka64@gmail.com>
2021-08-04 16:52:36 +0300

    aug_check

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -3,18 +3,12 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer');
 
-const fs = require('fs');
-
 const link = "http://localhost:5000/";
-
-// router.post('/signup', (req, res, next) => {
-//
-// });
 
 router.get('/login/check', (req, res) => {
     console.log(req.cookies);
     if (req.cookies.data) {
-        res.status(200).json({msg: "SUCCESS", c: req.cookies.data});
+        res.status(200).json({msg: "SUCCESS"});
     } else {
         res.status(404).json({error: "Data_Error"});
     }
@@ -22,14 +16,12 @@ router.get('/login/check', (req, res) => {
 
 router.post('/login', (req, res, next) => {
     if (req.body.data===process.env.ADMIN_PW) {
-        res.cookie(`data`, `yes`
-        //     , {
-        //     maxAge: 24 * 60 * 60 * 1000, // 24 hours,
-        //     secure: true,
-        //     httpOnly: true,
-        //     // sameSite: 'lax'
-        // }
-        );
+        res.cookie(`data`, `yes`, {
+            maxAge: 24 * 60 * 60 * 1000, // 24 hours,
+            secure: true,
+            httpOnly: true,
+            //sameSite: 'lax'
+        });
         res.status(200).json({msg: "SUCCESS"});
     } else {
         res.status(404).json({error: "Data_Error"});
